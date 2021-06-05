27033a04cace7f97ba1f1b34947c995e757f6ee1
ToxicFrazzles <frazzles740@gmail.com>
2021-06-05 14:03:42 +0100

    Revert "Add __str__ method to Robit model"
    This reverts commit a403b476

--- sample_project/sample_project/routing.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path, include
-from channels.routing import URLRouter
-import robitcontrol.routing
-
-
-websocket_urlpatterns = [
-    path('robitcontrol/', URLRouter(robitcontrol.routing.websocket_urlpatterns), name='robitcontrol')
-]
