1d7cc2d4659b7588d78b50fb3377b1c3add6c882
Frazer <frazzles740@gmail.com>
2021-08-12 17:01:07 +0100

    List available robots on control page

--- robitcontrol/views/index.py
+++ robitcontrol/views/index.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django import views
+from ..models import Robit
 
 
 class Index(views.View):
@@ -8,4 +9,8 @@ class Index(views.View):
         context = {
             "sock_url": "/robitcontrol" + reverse_lazy('browsersocket', urlconf='robitcontrol.routing')
         }
+        if request.user.is_superuser or request.user.is_staff:
+            context["robots"] = Robit.objects.exclude(channel_name=None)
+        else:
+            context["robots"] = Robit.objects.filter(available=True).exclude(channel_name=None)
         return render(request, 'robitcontrol/index.html', context=context)
