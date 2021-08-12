8cabd62816b3bc3132b87ffd60525e33a6af9349
Frazer <frazzles740@gmail.com>
2021-08-12 17:05:28 +0100

    Update admin to include channel name
    This lets me double check that robot websockets are correctly resetting the channel name when they disconnect.

--- robitcontrol/admin.py
+++ robitcontrol/admin.py
@@ -3,8 +3,8 @@ from .models import Robit
 
 
 class RobitAdmin(admin.ModelAdmin):
-    fields = ('name', 'update_bridge', 'key', 'available')
-    readonly_fields = ('key',)
+    fields = ('name', 'update_bridge', 'key', 'available', "channel_name")
+    readonly_fields = ('key', "channel_name")
 
 
 admin.site.register(Robit, RobitAdmin)
